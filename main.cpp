dc846f4f5dd51afcd47799b4936d30e6671a0ff5
chyhhwen <chyhhwen@gmail.com>
2023-01-05 17:25:59 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,35 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
+vector<int> line[100];
+int connect()
+{
+
+    return 0;
+}
 void fix()
 {
-   int put[3];
-   for(int i=0;i<3;i++)
-   {
-       cin >> put[i];
-   }
-   vector<int> man;
-   for(int i=1;i<=put[0];i++)
-   {
-       man.push_back(i);
-   }
-   int time = 0;
-   for(int i=0;i<=put[2];i++)//5
-   {
-       for(int j=0;j<put[1];j++)//2
-       {
-           if(time == (put[0] - i))
-           {
-               time -= (put[0] - i);
-           }
-           time++;
-       }
-       time --;
-       if(i == put[2])
-       {
-           cout << man[time];
-       }
-       man.erase(man.begin()+(time));
-   }
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            int push;
+            cin >> push;
+            line[i].push_back(push);
+        }
+    }
+    connect();
 }
 int main()
 {
